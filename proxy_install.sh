f8557f895cb49d0fca732ac701f66f7ed3caa6a9
Milos Vasic <milos85vasic@gmail.com>
2021-01-17 14:12:06 +0100

    MSF-395 - Enabling insecure mode for self signed CA proxy.

--- proxy_install.sh
+++ proxy_install.sh
@@ -9,5 +9,25 @@ isSelfSignedCA="$5"
 echo "Initializing Proxy"
 echo "Parameters(host=$host, port=$port, account=$account, password=$password, isSelfSignedCA=$isSelfSignedCA)"
 
+if [ -n "$isSelfSignedCA" ]; then
 
+  source=$(cat /root/.curlrc)
+  if ! echo "$source" | grep -i "insecure"; then
 
+    echo "Enabling 'Insecure' setting for Curl"
+    if echo insecure >>~/.curlrc; then
+
+      echo "Enabled 'Insecure' setting for Curl"
+    else
+
+      echo "ERROR: could not enable 'Insecure' setting for Curl"
+      exit 1
+    fi
+  else
+
+    echo "'Insecure' setting for Curl is already set"
+  fi
+else
+
+  echo "'Insecure' setting for Curl is not needed"
+fi
