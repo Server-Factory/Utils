e816cfbf926b840bdaaf3262f6e9e3d1d83f80f2
Milos Vasic <milos85vasic@gmail.com>
2021-01-17 14:04:16 +0100

    MSF-395 - Passing the parameter.

--- proxy_install.sh
+++ proxy_install.sh
@@ -4,7 +4,10 @@ host="$1"
 port="$2"
 account="$3"
 password="$4"
+isSelfSignedCA="$5"
 
 echo "Initializing Proxy"
-echo "Parameters(host=$host, port=$port, account=$account, password=$password)"
+echo "Parameters(host=$host, port=$port, account=$account, password=$password, isSelfSignedCA=$isSelfSignedCA)"
+
+
 
