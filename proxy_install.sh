c52ed7181d6a63f73e3ae04b8e271e4c8c09b1fa
Milos Vasic <milos85vasic@gmail.com>
2021-02-09 18:06:14 +0100

    MSF-395 - Disabling SELinux.

--- proxy_install.sh
+++ proxy_install.sh
@@ -102,9 +102,9 @@ WantedBy=multi-user.target""" | tee "$proxy_service" >/dev/null 2>&1 && chmod 64
     if "$setenforce_script" && systemctl enable "$proxy_service_file_name" && systemctl start "$proxy_service_file_name"; then
       echo "Proxy service started"
 
-      selinux_config_path="/etc/sysconfig"
-      selinux_config="$selinux_config_path/selinux"
-      selinux_config_backup="$selinux_config_path/selinux_old"
+      selinux_config_path="/etc/selinux"
+      selinux_config="$selinux_config_path/config"
+      selinux_config_backup="$selinux_config_path/config.bak"
       if test -e "$selinux_config"; then
 
           if ! test -e "$selinux_config_backup"; then
