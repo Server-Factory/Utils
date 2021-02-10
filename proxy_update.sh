4509cb1466ed36cf55adcd579bb2f297b667c61f
Milos Vasic <milos85vasic@gmail.com>
2021-02-10 14:55:26 +0100

    MSF-395 - Working on proxy update script.

--- proxy_update.sh
+++ proxy_update.sh
@@ -2,13 +2,27 @@
 
 working_directory="$1"
 log="$working_directory"/proxy_update.log
+config_file="$working_directory"/proxy.cfg
+
 date > "$log"
 
+if ! test -e "$config_file"; then
+
+  error="ERROR: $config_file does not exist"
+  echo "$error" >> "$log"
+  exit 1
+fi
+
+config=$(cat "$config_file")
+
+# shellcheck disable=SC2129
 echo "" >> "$log"
-config=$(cat "$working_directory"/proxy.cfg)
 echo "Configuration:" >> "$log"
 echo "$config" >> "$log"
+echo "" >> "$log"
 
+while read -r line; do
 
-# TODO: Load configuration from .cfg file
+  echo ">>> $line" >> "$log"
+done <"$config_file"
 
