5bada7574d96b9c55f9aab441c70e8282542127e
Milos Vasic <milos85vasic@gmail.com>
2021-02-11 19:32:07 +0100

    MSF-395 - Working on proxy update script.

--- proxy_update.sh
+++ proxy_update.sh
@@ -4,34 +4,37 @@ working_directory="$1"
 log="$working_directory"/proxy_update.log
 config_file="$working_directory"/proxy.cfg
 
-date > "$log"
+date >"$log"
 
 if ! test -e "$config_file"; then
 
   error="ERROR: $config_file does not exist"
-  echo "$error" >> "$log"
+  echo "$error" >>"$log"
   exit 1
 fi
 
 config=$(cat "$config_file")
-printf "\nConfiguration:\n\n%s\n\n" "$config" >> "$log"
+printf "\nConfiguration:\n\n%s\n\n" "$config" >>"$log"
 
 while read -r line; do
 
-  echo ">>> $line" >> "$log"
   export IFS="="
   parameter_name=""
   for parameter in $line; do
 
-    if [ -z "$parameter_name" ]
-    then
+    if [ "$parameter_name" = "" ]; then
 
-      echo ">>> Parameter name: $parameter" >> "$log"
       parameter_name="$parameter"
     else
 
-      echo ">>> Parameter value: $parameter" >> "$log"
+      eval "$parameter_name"="$parameter"
     fi
   done
 done <"$config_file"
 
+# shellcheck disable=SC2154,SC2129
+printf "Loaded parameters:\n\nhost=%s\nport=%s\naccount=%s\npassword=%s\n" "$host" "$port" "$account" "$password" >> "$log"
+# shellcheck disable=SC2154,SC2129
+printf "is_selfSigned_ca=%s\ncertificate_endpoint=%s\n" "$is_selfSigned_ca" "$certificate_endpoint" >> "$log"
+# shellcheck disable=SC2154,SC2129
+printf "delayed=%s\nutils=%s\n" "$delayed" "$utils" >> "$log"
