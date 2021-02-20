71b28413db05f067110887e7f208dd658fd91b4a
Milos Vasic <milos85vasic@gmail.com>
2021-02-20 15:57:56 +0100

    MSF-395 - Testing, fixing and polishing.

--- proxy_install.sh
+++ proxy_install.sh
@@ -98,96 +98,117 @@ if test -e "$proxy_service"; then
   fi
 fi
 
-if mv "$here"/Proxy/"$proxy_service_file_name" "$proxy_service" &&
-  test -e "$proxy_service" && chmod 640 "$proxy_service"; then
+systemd_service="/etc/systemd/system/$proxy_service_file_name"
+if test -e systemd_service; then
 
-  echo "$proxy_service: proxy service file saved"
-  if ! test -e "$set_enforce_script"; then
+  echo "$systemd_service already exists, cleaning up"
 
-    echo "ERROR: $set_enforce_script does not exits"
-    exit 1
+  if systemctl stop $proxy_service_file_name && \
+    systemctl disable $proxy_service_file_name >/dev/null 2>&1; then
+
+    echo "$systemd_service stopped and disabled"
   fi
 
-  systemd_service="/etc/systemd/system/$proxy_service_file_name"
-  if test -e systemd_service; then
+  if rm -f "$systemd_service"; then
 
-    echo "$systemd_service already exists, cleaning up"
-    if rm -f "$systemd_service"; then
-
-      echo "$systemd_service removed"
-    else
-
-      echo "ERROR: $systemd_service could not be removed"
-      exit 1
-    fi
-  fi
-
-  if cp "$proxy_service" "$systemd_service"; then
-
-    echo "$systemd_service is ready"
+    echo "$systemd_service removed"
   else
 
-    echo "ERROR: $proxy_service could not be created"
+    echo "ERROR: $systemd_service could not be removed"
     exit 1
   fi
+fi
 
-  if "$set_enforce_script" >/dev/null 2>&1 &&
-    systemctl enable "$proxy_service_file_name" &&
-    systemctl start "$proxy_service_file_name"; then
+validate_ip_script="$here/validate_ip_address.sh"
+if ! test -e "$validate_ip_script"; then
 
-    if systemctl status "$proxy_service_file_name" | grep running >/dev/null 2>&1; then
-
-      echo "Proxy service started"
-    else
-
-      echo "ERROR: Proxy service is not running"
-      exit 1
-    fi
-
-    selinux_config_path="/etc/selinux"
-    selinux_config="$selinux_config_path/config"
-    selinux_config_backup="$selinux_config_path/config.bak"
-
-    if sestatus | grep -i "disabled" >/dev/null 2>&1; then
-
-      echo "SELinux is already disabled"
-    else
-
-      if test -e "$selinux_config"; then
-
-        if ! test -e "$selinux_config_backup"; then
-
-          echo "$selinux_config: backing up"
-          if ! mv "$selinux_config" "$selinux_config_backup"; then
-
-            echo "ERROR: $selinux_config could not backup"
-            exit 1
-          fi
-        fi
-
-        if echo "SELINUX=disabled" >"$selinux_config" &&
-          echo "SELINUXTYPE=targeted" | tee -a "$selinux_config" >/dev/null 2>&1; then
-
-          echo "SELinux is disabled"
-        else
-
-          echo "ERROR: Could not disable SELinux"
-        fi
-      else
-
-        echo "WARNING: $selinux_config not found"
-      fi
-    fi
-  else
-
-    echo "ERROR: Could not start proxy service"
-    exit 1
-  fi
-else
-
-  echo "ERROR: $proxy_service proxy service file not saved"
+  echo "ERROR: $validate_ip_script does not exist"
   exit 1
 fi
+
+if sh "$validate_ip_script" "$host" >/dev/null 2>&1; then
+
+  echo "Proxy host is IP address: $host"
+else
+
+  if mv "$here"/Proxy/"$proxy_service_file_name" "$proxy_service" &&
+    test -e "$proxy_service" && chmod 640 "$proxy_service"; then
+
+    echo "$proxy_service: proxy service file saved"
+    if ! test -e "$set_enforce_script"; then
+
+      echo "ERROR: $set_enforce_script does not exits"
+      exit 1
+    fi
+
+    if cp "$proxy_service" "$systemd_service"; then
+
+      echo "$systemd_service is ready"
+    else
+
+      echo "ERROR: $proxy_service could not be created"
+      exit 1
+    fi
+
+    if "$set_enforce_script" >/dev/null 2>&1 &&
+      systemctl enable "$proxy_service_file_name" &&
+      systemctl start "$proxy_service_file_name"; then
+
+      if systemctl status "$proxy_service_file_name" | grep running >/dev/null 2>&1; then
+
+        echo "Proxy service started"
+      else
+
+        echo "ERROR: Proxy service is not running"
+        exit 1
+      fi
+
+      selinux_config_path="/etc/selinux"
+      selinux_config="$selinux_config_path/config"
+      selinux_config_backup="$selinux_config_path/config.bak"
+
+      if sestatus | grep -i "disabled" >/dev/null 2>&1; then
+
+        echo "SELinux is already disabled"
+      else
+
+        if test -e "$selinux_config"; then
+
+          if ! test -e "$selinux_config_backup"; then
+
+            echo "$selinux_config: backing up"
+            if ! mv "$selinux_config" "$selinux_config_backup"; then
+
+              echo "ERROR: $selinux_config could not backup"
+              exit 1
+            fi
+          fi
+
+          if echo "SELINUX=disabled" >"$selinux_config" &&
+            echo "SELINUXTYPE=targeted" | tee -a "$selinux_config" >/dev/null 2>&1; then
+
+            echo "SELinux is disabled"
+          else
+
+            echo "ERROR: Could not disable SELinux"
+          fi
+        else
+
+          echo "WARNING: $selinux_config not found"
+        fi
+      fi
+    else
+
+      echo "ERROR: Could not start proxy service"
+      exit 1
+    fi
+  else
+
+    echo "ERROR: $proxy_service proxy service file not saved"
+    exit 1
+  fi
+fi
+
 
 # TODO: Remove:
 echo "WORK IN PROGRESS"
