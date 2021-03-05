ddbb355647ea5990082f2a0e5bcaf9a365a7cb98
Milos Vasic <milos85vasic@gmail.com>
2021-03-05 20:19:32 +0100

    MSF-409 - Mute the output.

--- proxy_update_docker.sh
+++ proxy_update_docker.sh
@@ -6,4 +6,4 @@ proxy_home="$1"
 echo "Proxy update for Docker executed"
 echo "Parameters: here=$here proxy_home=$proxy_home"
 
-sh "$here/proxy_update.sh" "$proxy_home" &
+sh "$here/proxy_update.sh" "$proxy_home" >/dev/null 2>&1 &
