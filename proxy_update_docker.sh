d63a2f8dfd7382e0e2d517a6c0576bba888b5603
Milos Vasic <milos85vasic@gmail.com>
2021-03-25 14:03:41 +0100

    MSF-395 - Testing, fixing and polishing.

--- proxy_update_docker.sh
+++ proxy_update_docker.sh
@@ -3,7 +3,7 @@
 here=$(dirname "$0")
 proxy_home="$1"
 
-echo "Proxy update for Docker executed"
+echo "Proxy update for Docker executed at $(date)"
 echo "Parameters: here=$here proxy_home=$proxy_home"
 
 sh "$here/proxy_update.sh" "$proxy_home" >/dev/null 2>&1 &
