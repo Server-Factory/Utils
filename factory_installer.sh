8698ea05efa1f569a2fa7426a3327fce98a26df3
Milos Vasic <milos85vasic@gmail.com>
2021-04-04 13:27:15 +0200

    MSF-425 - Refactoring.

--- factory_installer.sh
+++ factory_installer.sh
@@ -19,69 +19,68 @@ fi
 if test Application/Release/Application.jar; then
 
   factoryPath="$2"
-  echo "Please enter your sudo password to install the software" &&
-    if sudo cp -f Application/Release/Application.jar "$factoryPath/factory_$factoryType.jar" &&
-      cp -f Core/Utils/factory.sh "$factoryPath/factory_$factoryType.sh"; then
+  if cp -f Application/Release/Application.jar "$factoryPath/factory_$factoryType.jar" &&
+    cp -f Core/Utils/factory.sh "$factoryPath/factory_$factoryType.sh"; then
 
-      definitions="$factoryPath/Definitions"
-      detailsJson="$definitions/Details.json"
-      if test -e "$detailsJson"; then
-        # shellcheck disable=SC2002
-        if cat "$detailsJson" | grep "repository_version" >/dev/null 2>&1; then
+    definitions="$factoryPath/Definitions"
+    detailsJson="$definitions/Details.json"
+    if test -e "$detailsJson"; then
+      # shellcheck disable=SC2002
+      if cat "$detailsJson" | grep "repository_version" >/dev/null 2>&1; then
 
-          echo "Existing software definitions found, cleaning up"
-          if sudo rm -rf "$definitions"; then
+        echo "Existing software definitions found, cleaning up"
+        if rm -rf "$definitions"; then
 
-            echo "Clean up completed"
-          else
-
-            echo "Clean up failed"
-            exit 1
-          fi
-        fi
-      fi
-
-      coreRoot="$factoryPath/Core"
-      coreUtils="$coreRoot/Utils"
-      coreUtilsReadme="$coreUtils/README.md"
-      if test -e "$coreUtilsReadme"; then
-        # shellcheck disable=SC2002
-        if cat "$coreUtilsReadme" | grep "Server Factory Utils" >/dev/null 2>&1; then
-
-          echo "Existing core utils found, cleaning up"
-          if sudo rm -rf "$coreUtils"; then
-
-            echo "Clean up completed"
-          else
-
-            echo "Clean up failed"
-            exit 1
-          fi
-        fi
-      fi
-
-      mkdir -p "$coreRoot"
-      if sudo cp -R Core/Utils "$coreRoot" && sudo chmod -R 750 "$coreRoot" && \
-      sudo chmod -R 750 "$coreRoot"; then
-
-        echo "Core utils have been installed with success"
-        if sudo cp -R Definitions "$definitions" && \
-          sudo chmod -R 550 "$definitions"; then
-
-          echo "Software has been installed with success"
+          echo "Clean up completed"
         else
 
-          echo "Software installation failed, could not copy software definitions"
+          echo "Clean up failed"
+          exit 1
         fi
+      fi
+    fi
+
+    coreRoot="$factoryPath/Core"
+    coreUtils="$coreRoot/Utils"
+    coreUtilsReadme="$coreUtils/README.md"
+    if test -e "$coreUtilsReadme"; then
+      # shellcheck disable=SC2002
+      if cat "$coreUtilsReadme" | grep "Server Factory Utils" >/dev/null 2>&1; then
+
+        echo "Existing core utils found, cleaning up"
+        if rm -rf "$coreUtils"; then
+
+          echo "Clean up completed"
+        else
+
+          echo "Clean up failed"
+          exit 1
+        fi
+      fi
+    fi
+
+    mkdir -p "$coreRoot"
+    if cp -R Core/Utils "$coreRoot" && chmod -R 750 "$coreRoot" &&
+      chmod -R 750 "$coreRoot"; then
+
+      echo "Core utils have been installed with success"
+      if cp -R Definitions "$definitions" &&
+        chmod -R 550 "$definitions"; then
+
+        echo "Software has been installed with success"
       else
 
-        echo "Core utils installation failed, could not copy files"
+        echo "Software installation failed, could not copy software definitions"
       fi
     else
 
-      echo "Software installation failed"
-      exit 1
+      echo "Core utils installation failed, could not copy files"
     fi
+  else
+
+    echo "Software installation failed"
+    exit 1
+  fi
 else
 
   echo "No Application.jar found"
